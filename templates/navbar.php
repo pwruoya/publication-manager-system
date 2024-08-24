f2c007922bc7d2f54eb2e0c4754305a4858cb1c5
pwruoya <pruoya@kabarak.ac.ke>
2024-08-24 19:39:16 +0300

    Updated color of navbar

--- templates/navbar.php
+++ templates/navbar.php
@@ -4,17 +4,43 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Publication System</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <style>
+    .navbar {
+      background-color: #003366; /* Navy blue background color */
+    }
+    .navbar-brand {
+      color: #fff !important;
+    }
+    .navbar-nav .nav-link {
+      color: #fff !important;
+      font-size: 16px;
+      transition: background-color 0.3s, color 0.3s;
+    }
+    .navbar-nav .nav-link:hover {
+      background-color: rgba(255, 255, 255, 0.3); /* Light hover effect */
+      border-radius: 5px;
+    }
+    .navbar-nav .nav-link.btn {
+      background-color: #dc3545; /* Red background for logout button */
+      padding: 5px 15px;
+      border-radius: 5px;
+    }
+    .navbar-nav .nav-link.btn:hover {
+      background-color: #c82333; /* Darker red for hover */
+    }
+  </style>
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light">
     <a class="navbar-brand" href="#">Publication System</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ml-auto">
+      <ul class="navbar-nav mr-auto">
         <li class="nav-item">
           <a class="nav-link" href="add_contributor.php">Add Contributor</a>
         </li>
@@ -30,12 +56,18 @@
         <li class="nav-item">
           <a class="nav-link" href="order.php">Order</a>
         </li>
+      </ul>
+      <ul class="navbar-nav ml-auto">
         <li class="nav-item">
-          <a class="nav-link" href="logout.php">Logout</a>
+          <a class="nav-link btn btn-danger text-white" href="logout.php">Logout</a>
         </li>
       </ul>
     </div>
   </nav>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
